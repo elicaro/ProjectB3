c583699c263a334660a21b7d2fd590e80cf845ba
carol <carol@Carolina>
2025-01-30 10:39:26 -0500

    Show you the logic

--- src/Demo1/DemoTest3.java
+++ src/Demo1/DemoTest3.java
@@ -1,5 +1,41 @@
 package Demo1;
 
 public class DemoTest3 {
+	
+	public static void main(String args[]) {
+
+		int temperature = 41;
+		String season = "Null";
+		String whatAmIThinking = "Null";
+
+		if (temperature >= 0 && temperature <= 20) {
+			season = "Winter ";
+			whatAmIThinking = "too cold, can’t wait for summer";
+
+		} else if (temperature >= 21 && temperature <= 36) {
+			season = "Fall ";
+			whatAmIThinking = "Confused, recovering from cold ";
+
+		} else if (temperature >= 41 && temperature <= 60) {
+			season = "Spring ";
+			whatAmIThinking = "More Confused, thinking about winter ";
+
+		} else if (temperature >= 61 && temperature <= 100) {
+			season = "Summer";
+			whatAmIThinking = "Too hot, can’t wait for winter ";
+
+		} else {
+			season = "Wrong Data && Java & Coffee";
+			whatAmIThinking = "Java & Coffe";
+
+			
+		}
+		System.out.println(temperature);
+		System.out.println(season);
+		System.out.println(whatAmIThinking);
+
+
+	}
+
 
 }
